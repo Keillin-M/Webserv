8dff815e69861c10cfa66e4ff054ce967ab4ac1a
Gabriel Souza <galeao.souza@outlook.com>
2026-02-05 16:53:27 +0100

    added the inical parsing

--- src/Request.cpp
+++ src/Request.cpp
@@ -3,24 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Request.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
+/*   By: gabrsouz <gabrsouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 12:57:41 by kmaeda            #+#    #+#             */
-/*   Updated: 2026/02/04 18:27:03 by kmaeda           ###   ########.fr       */
+/*   Updated: 2026/02/05 16:46:32 by gabrsouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Request.hpp"
+#include <sstream>
 
 Request::Request() {}
 
-// TODO: Friend implements this (Task 1 - parse first line)
+// Parse initial request line: METHOD SP PATH SP VERSION CRLF
 void Request::parse(const std::string& rawRequest) {
-	// Stub implementation - friend will replace this
-	(void)rawRequest;
-	method = "GET";  // Default for now
-	path = "/";      // Default for now
-	version = "HTTP/1.1";
+	method.clear();
+	path.clear();
+	version.clear();
+
+	std::string::size_type lineEnd = rawRequest.find("\r\n");
+	if (lineEnd == std::string::npos)
+		return;
+
+	std::string firstLine = rawRequest.substr(0, lineEnd);
+	std::istringstream iss(firstLine);
+	iss >> method >> path >> version;
 }
 
 std::string Request::getMethod() const { return method; }

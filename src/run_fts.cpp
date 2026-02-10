eec4da2c49c36083147dad3f451785bb7775792d
Gabriel Souza <galeao.souza@outlook.com>
2026-02-10 14:33:42 +0100

    took out the errno check in the accept ft

--- src/run_fts.cpp
+++ src/run_fts.cpp
@@ -6,7 +6,7 @@
 /*   By: gabrsouz <gabrsouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 12:24:17 by gabrsouz          #+#    #+#             */
-/*   Updated: 2026/02/10 12:40:02 by gabrsouz         ###   ########.fr       */
+/*   Updated: 2026/02/10 14:32:26 by gabrsouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,11 @@ static void iniciateAddr(struct sockaddr_in& addr, int port) {
 
 void Server::setupListenSocket() {
 	struct sockaddr_in addr;
-	
 	listenFd = socket(AF_INET, SOCK_STREAM, 0); //ipv4, TCP, default
 	if (listenFd < 0) {
 		std::cerr << "socket error: " << strerror(errno) << std::endl;
 		return;
 	}
-	
-	// Allow immediate restart
 	int opt = 1;
 	setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 	
@@ -38,16 +35,12 @@ void Server::setupListenSocket() {
 		close(listenFd);
 		listenFd = -1;
 		return;
-	}
-	if (listen(listenFd, SOMAXCONN) < 0) {
+	} if (listen(listenFd, SOMAXCONN) < 0) {
 		std::cerr << "listen error on port " << port << ": " << strerror(errno) << std::endl;
 		close(listenFd);
 		listenFd = -1;
 		return;
-	}
-
-	// (subject) "non-blocking at all times"
-	int flags = fcntl(listenFd, F_GETFL, 0);
+	} int flags = fcntl(listenFd, F_GETFL, 0);
 	fcntl(listenFd, F_SETFL, flags | O_NONBLOCK);
 }
 
@@ -56,7 +49,6 @@ void Server::createPollFds(std::vector<struct pollfd>& pollFds, struct pollfd pf
 	pfd.events = POLLIN;
 	pfd.revents = 0;
 	pollFds.push_back(pfd);
-	// add each client fd — state-aware event registration
 	std::map<int, Client>::const_iterator it;
 	for (it = clients.begin(); it != clients.end(); ++it) {
 		pfd.fd = it->first;
@@ -75,18 +67,14 @@ void Server::createPollFds(std::vector<struct pollfd>& pollFds, struct pollfd pf
 void Server::acceptNewClients() {
 	while (true) {
 		int clientFd = accept(listenFd, NULL, NULL);
-		if (clientFd < 0) {
-			if (errno == EWOULDBLOCK || errno == EAGAIN)
-				break; // no more connections waiting
-			std::cerr << "accept error: " << strerror(errno) << std::endl;
-			break;
-		}
-		// (subject) "non-blocking at all times"
+		if (clientFd > 0) {
 		int clientFlags = fcntl(clientFd, F_GETFL, 0);
 		fcntl(clientFd, F_SETFL, clientFlags | O_NONBLOCK);
 		clients.insert(std::make_pair(clientFd, Client(clientFd)));
 		clients.find(clientFd)->second.setState(READING);
 		std::cout << "[Server] Client connected: fd=" << clientFd << std::endl;
+		} else
+			break;
 	}
 }
 

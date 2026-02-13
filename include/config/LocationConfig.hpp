464d5078e42520b4aa6ff035c6dca6e151798da2
kmaeda <kmaeda@student.42berlin.de>
2026-02-13 12:21:15 +0100

    Add CGI configuration options to LocationConfig

--- include/config/LocationConfig.hpp
+++ include/config/LocationConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: kmaeda <kmaeda@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 14:19:12 by kmaeda            #+#    #+#             */
-/*   Updated: 2026/02/09 18:17:33 by kmaeda           ###   ########.fr       */
+/*   Updated: 2026/02/13 12:17:51 by kmaeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,30 @@
 class LocationConfig {
 	private:
 		std::string path;
-		std::string cgiPath;
+		std::string CGIPath;
 		std::string root;
 		std::string indexFile;
 		std::vector<std::string> allowedMethods;
-		std::vector<std::string> cgiExtensions;
+		std::vector<std::string> CGIExtensions;
 	
 	public:
 		LocationConfig();
 
 		// Getters
 		std::string getPath() const;
-		std::string getCgiPath() const;
 		std::vector<std::string> getAllowedMethods() const;
-		std::vector<std::string> getCgiExtensions() const;
 		std::string getRoot() const;
 		std::string getIndexFile() const;
+		std::vector<std::string> getCGIExtensions() const;
+		std::string getCGIPath() const;
 
 		// Setters
 		void setPath(const std::string& path);
-		void setCGIPath(const std::string& cgiPath);
 		void addAllowedMethods(const std::string& method);
-		void setCgiExtensions(const std::string& cgiExtension);
 		void setRoot(const std::string& root);
 		void setIndexFile(const std::string& index);
+		void setCGIExtensions(const std::string& CGIExtensions);
+		void setCGIPath(const std::string& CGIPath);
 
 		bool isMethodAllowed(const std::string& method) const;
 		void validate();

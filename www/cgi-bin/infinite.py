78e4e11dbd3012f3d1d786f7f7773f619f1bfa99
Tania Mara <taniamaramarcosadv@gmail.com>
2026-02-16 16:04:50 +0100

    CGI implementation - browser - frontend test page

---
+++ www/cgi-bin/infinite.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# CGI script with infinite loop - tests server timeout handling
+
+import time
+
+print("Content-Type: text/html\r")
+print("\r")
+print("<h1>Starting infinite loop...</h1>")
+
+# Infinite loop - server should timeout
+while True:
+    time.sleep(1)
